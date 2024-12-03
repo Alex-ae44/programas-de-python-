ddc4ca207384d4e9e185b6714bcdc7882054ce55
Alex-ae44 <alexacosta53674@gmail.com>
2024-12-03 00:34:38 -0600

    Create p-9.py

---
+++ codigo/bloque 2/p-9.py
@@ -0,0 +1,10 @@
+#Fecha: 
+#Realizado por: alejandro acosta
+art = int(input("digite el numero de articulos:  "))
+
+if art <= 3:
+    costo = art * 45
+else:
+    costo = art * 35
+print("El costo total es de:", costo)
+print("gracias por usar el programa")
