c24e2df9504a8fe6ec8d973cc40d913e15d9e949
Alex-ae44 <alexacosta53674@gmail.com>
2024-12-03 00:20:31 -0600

    Create p-6.py

---
+++ codigo/bloque 1/p-6.py
@@ -0,0 +1,7 @@
+#Fecha:
+#Realizado por: alejandro acosta
+
+base = float(input("Cuanto mide la base: "))
+altura= float(input("Cuanto mide la altura: "))
+area = base * altura / 2
+print("El area de este triangulo es: "+ str(area))
