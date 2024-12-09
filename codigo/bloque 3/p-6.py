b157e979a8e53b43c65c1de588db6ef86f84d324
ALEJANDRO AR <alexacosta53674@gmail.com>
2024-12-08 21:41:57 -0600

    Create p-6.py

---
+++ codigo/bloque 3/p-6.py
@@ -0,0 +1,15 @@
+
+# Fecha: 2024/10/29
+# Elaborado por: alejandro acosta
+
+print("Lista de enteros ") 
+print([40, 20, 35])
+
+print("\nLista de cadenas de texto") 
+print("Manzana", "piña", "Naranja")
+
+print("\nLista de booleanos")
+print([False, True, True])
+
+print("\nLista vacia") 
+print([])
