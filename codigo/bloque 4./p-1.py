2dc9e63d86f2606a06e6cdc23a901483adb00551
ALEJANDRO AR <alexacosta53674@gmail.com>
2024-12-08 21:59:47 -0600

    Create p-1.py

---
+++ codigo/bloque 4./p-1.py
@@ -0,0 +1,14 @@
+
+# Fecha: 2024/11/4
+# Elaborado por:alejandro acosta
+lista_num = [10, 30, 40, 20, 35, 80]
+print(lista_num[0])
+print(lista_num[1])
+print(lista_num[2])
+print(lista_num[3])
+print(lista_num[4])
+print(lista_num[5])
+
+# Haciendo lo mismo pero con un ciclo "for"
+for i in lista_num:
+    print(i)
