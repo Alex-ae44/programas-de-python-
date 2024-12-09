e41ba1bdae5562a4c38cb74766a88d4e2ec3a32f
ALEJANDRO AR <alexacosta53674@gmail.com>
2024-12-08 21:53:26 -0600

    Create p-11.py

---
+++ codigo/bloque 3/p-11.py
@@ -0,0 +1,10 @@
+
+# Fecha: 2024/10/31
+# Elaborado por:alejandro acosta
+
+nombres = ["jhon", "arthur", "sanchez"]
+
+print("Luis" in nombres) #True
+print("Emi" in nombres) #False
+print("Javier" in nombres) #False
+print("José" not in nombres) #True
