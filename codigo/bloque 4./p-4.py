5e30c379d9764d71fffb1a7dde9f9c4ccbe8edc0
ALEJANDRO AR <alexacosta53674@gmail.com>
2024-12-08 22:03:49 -0600

    Create p-4.py

---
+++ codigo/bloque 4./p-4.py
@@ -0,0 +1,9 @@
+
+# Fecha: 2024/11/4
+# Elaborado por: alejandro acosta
+
+personajes_favoritos = ["arthur morgan", "cj", "batman", "Iron Man", "Capitán América", "Thor",
+"Wonder Woman", "deadpool", "Lara Jean", "Flash"]
+
+for personaje in personajes_favoritos:
+    print(personaje)
