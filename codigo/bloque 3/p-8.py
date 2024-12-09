99c3656873bb911469bb97751d666c026918086f
ALEJANDRO AR <alexacosta53674@gmail.com>
2024-12-08 21:50:06 -0600

    Create p-8.py

---
+++ codigo/bloque 3/p-8.py
@@ -0,0 +1,15 @@
+
+# Fecha: 2024/10/31
+# Elaborado por: alejandro acosta
+print("Indexing")
+numeros = [10, 20, 30]
+print(numeros[2])
+print(numeros[0])
+print(numeros[1])
+print(numeros[-1])
+
+print("\nSubscripting")
+nombres = ["Chorchis", "Choto", "Emiliano", "Pepe el toro"]
+print(nombres[1:])
+print(nombres[1:5])
+print(nombres[-2:])
