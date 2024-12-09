d01b5f39aca1eabf4488087b0309e78a2548e807
ALEJANDRO AR <alexacosta53674@gmail.com>
2024-12-08 21:39:46 -0600

    Create p-3.py

---
+++ codigo/bloque 3/p-3.py
@@ -0,0 +1,11 @@
+
+# Fecha: 2024/10/28
+# Elaborado por:alejandro acosta
+variable = input("Ingresa tu edad: ")
+print(type(variable))
+
+variable = int(variable)     # Convierto la variable a tipo entero
+print(type(variable))
+
+variable = float(variable)   # Convierto la variable a tipo flotante
+print(type(variable))
