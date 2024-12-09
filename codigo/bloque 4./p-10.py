cf9307eb3697880d99f09705d00d43f3c8a37d18
ALEJANDRO AR <alexacosta53674@gmail.com>
2024-12-08 22:11:04 -0600

    Create p-10.py

---
+++ codigo/bloque 4./p-10.py
@@ -0,0 +1,12 @@
+
+# Fecha: 2024/11/8
+# Elaborado por:alejandro acosta
+
+# Definimos una cadena de texto
+cadena = "Python es un lenguaje de programacón"
+print(cadena)
+cadena_mayusculas = cadena.upper()
+print(cadena_mayusculas)
+
+cadena_minusculas = cadena.lower()
+print(cadena_minusculas)
