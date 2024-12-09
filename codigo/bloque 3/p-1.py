74e0d212a16c8bfb4a658dcc6cde905c067dc898
ALEJANDRO AR <alexacosta53674@gmail.com>
2024-12-08 21:38:00 -0600

    Update p-1.py

--- codigo/bloque 3/p-1.py
+++ codigo/bloque 3/p-1.py
@@ -1,1 +1,14 @@
 
+# Fecha: 2024/10/28
+# Elaborado por: alejandro acosta
+
+nombre = input("Ingresa el tipo de mascota: ")
+if "perro" in nombre:
+    print("Es un perro")
+
+elif "gato" in nombre: 
+    print("Es un gato")
+else:
+     print("Mascota desconocida")
+
+print("¡Gracias por usar nuestro programa!")
