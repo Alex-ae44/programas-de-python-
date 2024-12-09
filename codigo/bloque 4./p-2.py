ea324a2a10224e28d5d4f6625c7bab54fbd24d09
ALEJANDRO AR <alexacosta53674@gmail.com>
2024-12-08 22:00:57 -0600

    Create p-2.py

---
+++ codigo/bloque 4./p-2.py
@@ -0,0 +1,13 @@
+
+# Fecha: 2024/11/4
+# Elaborado por: alejandro acosta
+
+nombres = ["juana", "axel", "alejandro"]
+
+for nombre in nombres:
+    print("El nombre es:", nombre)
+
+frutas = ["Manzana", "Piña", "Plátano"]
+
+for fruta in frutas:
+    print("La fruta es:", fruta)
